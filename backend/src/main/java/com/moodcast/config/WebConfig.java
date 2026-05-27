ab8313227132564f8583a6a060e0058404f1c9ed
gh07003 <gh07003@gmail.com>
2026-05-28 00:39:55 +0900

    ec2, s3

--- backend/src/main/java/com/moodcast/config/WebConfig.java
+++ backend/src/main/java/com/moodcast/config/WebConfig.java
@@ -11,7 +11,8 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5173",
-                                "http://127.0.0.1:5173")
+                                "http://127.0.0.1:5173",
+                        "http://moodcast-frontend-s3-qqqq.s3-website.ap-northeast-2.amazonaws.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
